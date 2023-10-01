91e2f7fed17d74236234852f56e439ee39d31c15
Fred Code <frederico.diniz.rufino@gmail.com>
2023-10-01 18:04:02 -0300

    creating base project

---
+++ src/components/Product/index.tsx
@@ -0,0 +1,15 @@
+
+import { memo } from 'react';
+// import { IconButton } from '@material-ui/core';
+// import AddIcon from '@material-ui/icons/Add';
+// import RemoveIcon from '@material-ui/icons/Remove';
+
+
+function Produto() {
+  return (
+      <>
+      </>
+  )
+}
+
+export default memo(Produto)
